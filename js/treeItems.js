0b881ef39919e15dcf4385b58c29c71ba7bbf42a
Cory Sayles <cory.sayles@gmail.com>
2017-12-07 00:54:31 -0500

    Non-Blog updates

--- js/treeItems.js
+++ js/treeItems.js
@@ -7,32 +7,65 @@ var CORY = CORY || {};
 CORY.Tree = (function () {
    var coryNg = angular.module('coryBlog', ['ui.tree']);
    
-   coryNg.controller('CoryTreeController', ['$scope', function ($scope) {
+   coryNg.controller('CoryTechController', ['$scope', function ($scope) {
       $scope.treeList = [{
          "id" : 1,
-         "title" : "Tree Item 1",
+         "title" : "AngularJS",
          "nodes" : [
             {
                "id" : 1.1,
-               "title" : "Tree Item 1.1",
+               "title" : "ReactJS",
                "nodes" : []
             },
             {
                "id" : 1.2,
-               "title" : "Tree Item 1.2",
+               "title" : "CZML",
                "nodes" : []
+            },
+            {
+               "id" : 1.3,
+               "title" : "Python",
+               "nodes" : [{
+                     "id" : 1.4,
+                     "title" : "Minecraft Pi",
+                     "nodes" : []
+               }]
             }]
          }, {
             "id" : 2,
-            "title" : "Tree Item 2",
+            "title" : "Docker",
             "nodes" : []
          }, {
             "id" : 3,
-            "title" : "Tree Item 3",
+            "title" : "Google Cloud Platform (GCP)",
             "nodes" : []
          }, {
             "id" : 4,
-            "title" : "Tree Item4",
+            "title" : "Amazon Web Services (AWS)",
+            "nodes" : []
+         }, {
+            "id" : 5,
+            "title" : "Kubernetes",
+            "nodes" : []
+         }, {
+            "id" : 6,
+            "title" : "Git",
+            "nodes" : []
+         }, {
+            "id" : 7,
+            "title" : "gitpitch",
+            "nodes" : []
+         }, {
+            "id" : 8,
+            "title" : "SVG",
+            "nodes" : []
+         }, {
+            "id" : 9,
+            "title" : "Raspberry Pi",
+            "nodes" : []
+         }, {
+            "id" : 10,
+            "title" : "OpenMap",
             "nodes" : []
          }
       ];
